749d6ee2a96e58759af394676f069b9e0e67d6b5
Zulfadli Rizal <dev@bprbangunarta.co.id>
2024-01-15 14:38:14 +0700

    Add Layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SIPEBRI - Login Page</title>
+    <title>Login Page</title>
     <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}">
 
     {{-- Meta Description --}}
